fc27982bbf2d489af6570983bff3ce7a2f5f517e
federbetta92 <fge3492@gmail.com>
2026-07-21 12:51:54 -0300

    Refactor App component to include ProductProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,35 @@
 import { Routes, Route } from "react-router-dom"
 import { CartProvider } from "./context/CartContext"
 import { CategoryProvider } from "./context/CategoryContext"
+import { ProductProvider } from "./context/ProductContext"
+
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import WhatsAppButton from "./components/WhatsAppButton";
+import WhatsAppButton from "./components/WhatsAppButton"
+
 import Home from "./pages/Home"
 import Checkout from "./pages/Checkout"
-import OrderSuccess from "./pages/OrderSuccess";
+import OrderSuccess from "./pages/OrderSuccess"
 
 export default function App() {
   return (
-    <CartProvider>
-<CategoryProvider>
-      <Header />
+    <ProductProvider>
+      <CategoryProvider>
+        <CartProvider>
+          <Header />
 
-      <Routes>
-         <Route path="/" element={<Home />} />
-         <Route path="/categoria/:nombre" element={<Home />} />
-        <Route path="/categoria/:categoria/:subcategoria" element={<Home />} />
-        <Route path="/checkout" element={<Checkout />} />        
-        <Route path="/pedido-confirmado" element={<OrderSuccess />} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/categoria/:nombre" element={<Home />} />
+            <Route path="/categoria/:categoria/:subcategoria" element={<Home />} />
+            <Route path="/checkout" element={<Checkout />} />        
+            <Route path="/pedido-confirmado" element={<OrderSuccess />} />
+          </Routes>
 
-      </Routes>
-
-      <Footer />
-     <WhatsAppButton />
-</CategoryProvider>
-    </CartProvider>
+          <Footer />
+          <WhatsAppButton />
+        </CartProvider>
+      </CategoryProvider>
+    </ProductProvider>
   )
 }
