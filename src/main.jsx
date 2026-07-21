d9d146b74cefcbf7b3fbc9ec44e3393343a0e418
federbetta92 <fge3492@gmail.com>
2026-07-21 12:38:08 -0300

    Refactor main.jsx to use context providers

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
-import App from "./App"
-import "./index.css"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+// Importamos ambos proveedores
+import { CartProvider } from './context/CartContext'
+import { ProductProvider } from './context/ProductContext'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ProductProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </ProductProvider>
+  </React.StrictMode>
 )
